862289ea2f13669597eefad75b0ee09e02e0f645
whatsmysuggestion <whatsmysuggestion@gmail.com>
2022-07-30 09:07:30 +0530

    Spring Boot MVC Example

---
+++ src/main/java/com/kkar/app/controller/MvcConfiguration.java
@@ -0,0 +1,23 @@
+/*
+ * package com.kkar.app.controller;
+ * 
+ * import org.springframework.context.annotation.*; import
+ * org.springframework.web.servlet.config.annotation.*; import
+ * org.springframework.web.servlet.view.*;
+ * 
+ * 
+ * @Configuration
+ * 
+ * @EnableWebMvc
+ * 
+ * @ComponentScan public class MvcConfiguration extends WebMvcConfigurerAdapter
+ * {
+ * 
+ * @Override public void configureViewResolvers(ViewResolverRegistry registry) {
+ * InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+ * resolver.setPrefix("/WEB-INF/view/"); resolver.setSuffix(".jsp");
+ * resolver.setViewClass(JstlView.class); registry.viewResolver(resolver); }
+ * 
+ * 
+ * }
+ */
